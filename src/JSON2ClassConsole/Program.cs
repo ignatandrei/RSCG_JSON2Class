c9aed3e163e83bd5ca593bdae668b5357025a425
Andrei Ignat <ignatandrei@yahoo.com>
2024-02-29 08:08:53 +0200

    Update JSON2ClassConsole project configuration and add CD

--- src/JSON2ClassConsole/Program.cs
+++ src/JSON2ClassConsole/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System.Text.Json;
-Console.WriteLine("Hello, World!");
 var testData = JsonSerializer.Deserialize<JSON2ClassConsole.SettingsJson.testData>(System.IO.File.ReadAllText("testData.json"));
 ArgumentNullException.ThrowIfNull(testData);
+Console.WriteLine(testData.Logging.LogLevel.Default);
 Console.WriteLine(testData.DictData.Number_2);
